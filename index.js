5e2535370c622bb2aa96d48fd2492d0a02e40fb5
devWRM <mcwen@comcast.net>
2020-11-21 14:26:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+function submitData(name, email) {
+
+// makes a POST request to /user with a name and email
+
+     return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name,
+    email
+  })
+});
+
+
+
+// handles the POST request response, retrieves the new id value and appends it to the DOM:
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
