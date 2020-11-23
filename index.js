6efd2ea42f60d9068683838684b4278f0e459a76
Eli Cusic <tntitans31@gmail.com>
2020-11-23 17:19:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(username, email){
+    const info = {
+        name: username,
+        email: email
+    }
+
+    const configObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify(info)
+    }
+
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(json){
+            let id = json["id"]
+            const div = document.createElement("div")
+            div.innerHTML = id
+            document.body.appendChild(div)
+        })
+        .catch(function(error){
+            let msg = document.createElement("p")
+            msg.innerHTML = error.message
+            document.body.appendChild(msg)
+        })
+}
